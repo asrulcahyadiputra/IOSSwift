d3f78beee6d2a813b07c49d7ce6270e61d0dc5e4
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-01-20 06:46:57 +0700

    Myfirst APP

--- MyFirst/ViewController.swift
+++ MyFirst/ViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtTinggi: UITextField!
+    @IBOutlet weak var txtAlas: UITextField!
+    @IBAction func btnHitung(_ sender: UIButton) {
+        let alas = txtAlas.text!
+        let tinggi = txtTinggi.text!
+        
+        
+        //convert to float
+        let floatalas = Float(alas)!
+        let floatTInggi = Float(tinggi)!
+        
+        // result
+        let hasil = 0.5 * floatalas * floatTInggi
+        
+        txtHasil.text = String(hasil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var txtHasil: UILabel!
 
 
 }
