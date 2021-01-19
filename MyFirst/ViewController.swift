d090d3e2ef85e3396226ae2bf3de1771dd9e9963
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-01-20 05:35:12 +0700

    Initial Commit

---
+++ MyFirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirst
+//
+//  Created by Asrul Cahyadi Putra on 20/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
